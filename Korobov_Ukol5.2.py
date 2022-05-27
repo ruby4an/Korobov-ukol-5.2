c8294bcf6bc17ae5b2d7e5db72a952291f5d992e
ruby4an <104034510+ruby4an@users.noreply.github.com>
2022-05-27 13:57:13 +0200

    Add files via upload

---
+++ Korobov_Ukol5.2.py
@@ -0,0 +1,9 @@
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+x = int(input("Input x:"))
+print("factorial of", x, "equals to", factorial(x))
